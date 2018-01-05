af345e8fb662f37d32167c38702a0eaca01ae235
Anwana Ntofon <anwanantofon@gmail.com>
2018-01-04 18:29:22 -0800

    Add same functionality for Offline Button
    To be modified later. Determine how the 'All' and 'Offline' functionality display to the screen.

--- script.js
+++ script.js
@@ -25,4 +25,23 @@ $(document).ready(function() {
     });
   });
 
-});
+  $offlineButton.click(function() {
+    $.getJSON(esl_sc2, function(data) {
+      if (data.stream === null) {
+        console.log("ESL_SC2 offline");
+      } else {
+        console.log("ESL_SC2 online");
+        console.log(data);
+      }
+
+    });
+    $.getJSON(freecodecamp_URL, function(data) {
+      if (data.stream === null) {
+        console.log("freecodecamp offline");
+      } else {
+        console.log("freecodecamp online");
+        console.log(data);
+      }
+    });
+
+  });
