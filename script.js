d9a9c9ce974513e824a1556c4ba147b46a82218d
Anwana Ntofon <anwanantofon@gmail.com>
2018-01-04 17:50:18 -0800

    Began script file with tab button vars
    Will perform actions upon user click of each tab.

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  "use strict";
+
+  var $allButton = $('#all-button');
+  var $onlineButton = $('#online-button');
+  var $offlineButton = $('#offline-button');
+
+});
