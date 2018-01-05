72bb73b0235d4f390130b8a360bdd3269472dbe0
Anwana Ntofon <anwanantofon@gmail.com>
2018-01-04 18:28:18 -0800

    Print status of two channels
    If Online, print 'online' and the channels' data to console. Else print 'offline' to console.

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ $(document).ready(function() {
   var $onlineButton = $('#online-button');
   var $offlineButton = $('#offline-button');
 
-  $allButton.click(function() {});
+  $allButton.click(function() {
+    $.getJSON(esl_sc2, function(data) {
+      if (data.stream === null) {
+        console.log("ESL_SC2 offline");
+      } else {
+        console.log("ESL_SC2 online");
+        console.log(data);
+      }
+
+    });
+    $.getJSON(freecodecamp_URL, function(data) {
+      if (data.stream === null) {
+        console.log("freecodecamp offline");
+      } else {
+        console.log("freecodecamp online");
+        console.log(data);
+      }
+    });
+  });
 
 });
